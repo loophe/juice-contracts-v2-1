02332a4f764b44e97d613e59bdfbdf7dab6f67fd
loophe <loophe@163.com>
2022-03-27 00:09:13 +0800

    from et to ec

--- brownie/contracts/libraries/JBSplitsGroups.sol
+++ brownie/contracts/libraries/JBSplitsGroups.sol
@@ -4,4 +4,5 @@ pragma solidity 0.8.6;
 library JBSplitsGroups {
   uint256 public constant ETH_PAYOUT = 1;
   uint256 public constant RESERVED_TOKENS = 2;
+  uint256 public constant DAI_PAYOUT = 3;
 }
--- brownie/scripts/token_local.py
+++ brownie/scripts/token_local.py
@@ -6,9 +6,9 @@ def main():
    
     USD = interface.daiInterface('0x6b175474e89094c44da98b954eedeac495271d0f') #DAI mainnet
     FUND = 1000000e18
-    # USD.mint(accounts[0],FUND,{'from':'0x9759A6Ac90977b93B58547b4A71c78317f391A28'})
+    USD.mint(accounts[0],FUND,{'from':'0x9759A6Ac90977b93B58547b4A71c78317f391A28'})
     balU = USD.balanceOf(accounts[0])
-    print(f'User has USD balance is {balU}\n')
+    print(f'User has USD balance is {balU/1e18}\n')
     os = JBOperatorStore.deploy({'from':accounts[0]})
     pc = JBPrices.deploy(accounts[0],{'from':accounts[0]})
     pr = JBProjects.deploy(os,{'from':accounts[0]})
@@ -22,12 +22,13 @@ def main():
     # et = JBETHPaymentTerminal.deploy(1,os,pr,dr,ps,pc,pt,accounts[0],{'from':accounts[0]})
    
     print(f'ERC20PaymentTermianl deploying...')
-    ec = JBERC20PaymentTerminal.deploy(USD,2,1,2,os,pr,dr,ps,pc,pt,accounts[0],{'from':accounts[0]})
+    ec = JBERC20PaymentTerminal.deploy(USD,2,1,3,os,pr,dr,ps,pc,pt,accounts[0],{'from':accounts[0]})
     print(f'ERC20PaymentTerminal has deployed at {ec}\n')
     lc = JBCurrencies.deploy({'from':accounts[0]})    
     ch = JBChainlinkV3PriceFeed.deploy('0x773616E4d11A78F511299002da57A0a94577F1f4',{'from':accounts[0]}) #Mainnet fork
     pc.addFeedFor(lc.USD(),lc.ETH(),ch,{'from':accounts[0]})
-    cr.launchProjectFor(accounts[0],['',0],[1209600,1e25,40000000,'0x0000000000000000000000000000000000000000'],[5000,7000,7000,False,False,False,False,False,False,False,False,False,False,False,False,'0x0000000000000000000000000000000000000000'],0,[],[],[et],'',{'from':accounts[0]})
+    # cr.launchProjectFor(accounts[0],['',0],[1209600,1e25,40000000,'0x0000000000000000000000000000000000000000'],[5000,7000,7000,False,False,False,False,False,False,False,False,False,False,False,False,'0x0000000000000000000000000000000000000000'],0,[],[],[et],'',{'from':accounts[0]})
+    cr.launchProjectFor(accounts[0],['',0],[1209600,100e18,40000000,'0x0000000000000000000000000000000000000000'],[5000,7000,7000,False,False,False,False,False,False,False,False,False,False,False,False,'0x0000000000000000000000000000000000000000'],0,[],[],[ec],'',{'from':accounts[0]})
     balp = pr.balanceOf(accounts[0])
     print(f'Controller launch project {balp}.\n')
     # et.pay(0,1,accounts[0],0,False,'loophe','0x00',{'from':accounts[0],'value':10e18})
